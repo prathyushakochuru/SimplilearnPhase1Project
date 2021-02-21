d6a1a9b2663b5974784be553f7f84fef5cb63ece
Prathyusha Kochuru <kochuruprathyusha@gmail.com>
2021-02-21 08:32:35 -0500

    Updated the code & specifucation document

--- SimplilearnPhase1Assessment/LockedMeProject/src/UserOperations/Utilityoperations.java
+++ SimplilearnPhase1Assessment/LockedMeProject/src/UserOperations/Utilityoperations.java
@@ -1,17 +1,36 @@
 package UserOperations;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Utilityoperations {
 	
+	public static int mainMenu()
+	{
+		System.out.println("                 ****Welcome to LockedMe.com!****               ");
+		System.out.println("              Where you find the exclusive fineart           ");
+		System.out.println("Hello user, I am Prathyusha Kochuru - Full Stack Web Developer, Lockers Pvt. Ltd.");
+		System.out.println("Please select the folliwing options:");
+		System.out.println("1. Retrieve the filenames");
+		System.out.println("2. Perform CRUD file operations");
+		System.out.println("3. Navigate to the main menu");
+		
+		int op;
+		Scanner scanner = new Scanner(System.in);
+		op = scanner.nextInt();
+		return op;
+	}
+	
 	public static List<String> getFilenames(String strfolder) 
 	{
-		List<String> fileslist = new ArrayList<String>();
+		List<String> fileslist = new LinkedList<String>();
 		File[] filenames = new File(strfolder).listFiles();
 		
 		for(File file:filenames)
@@ -30,16 +49,19 @@ public class Utilityoperations {
 		
 	}
 	
-	public static void addFile(String strfolder, String strfilename) throws FileNotFoundException
+	public static void addFile(String strfolder, String strfilename, String strwrite) throws IOException
 	{
-		String strnewfile = strfolder.trim() + "/" + strfilename.trim();
+		String strnewfile = strfolder.trim() + "/" + strfilename.trim().toLowerCase();
 		
-		File newfile = new File(strnewfile);
+		File newfile = new File(strnewfile.toLowerCase());
+		FileWriter filewrite = new FileWriter(strnewfile);
 		
 		try {
-			if(newfile.createNewFile())
+			if(newfile.exists())
 			{
-				System.out.println(strfilename + " created SUCCESSFULLY in " + strfolder);
+				filewrite.write(strwrite);
+				filewrite.close();
+				System.out.println(strfilename + " created and wrote SUCCESSFULLY in " + strfolder);
 			}
 			else
 			{
@@ -49,6 +71,30 @@ public class Utilityoperations {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		finally
+		{
+			filewrite.close();
+			
+		}
+	}
+	
+	protected static void delFile(String strfolder, String strfilename) throws SecurityException 
+	{
+		String strnewfile = strfolder.trim() + "/" + strfilename.trim();
+		File file = new File(strnewfile);
+		
+		if(file.exists())
+		{
+			file.delete();
+			System.out.println(strfilename + " is successfully deleted!");	
+		}
+		else
+		{
+			System.out.println("Sorry, "+ strfilename + " file not found!");
+		}
+		
+		
+		
 	}
 
 }
--- SimplilearnPhase1Assessment/LockedMeProject/src/UserOperations/WebUserOperations.java
+++ SimplilearnPhase1Assessment/LockedMeProject/src/UserOperations/WebUserOperations.java
@@ -1,34 +1,37 @@
 package UserOperations;
+import java.lang.String;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 
 public class WebUserOperations {
 
+	private static boolean String;
+
 	public static void main(String[] args) {
 		
-
-		System.out.println("                 ****Welcome to LockedMe.com!****               ");
-		System.out.println("              Where you find the exclusive fineart           ");
-		System.out.println("Hello user, I am Prathyusha Kochuru - Full Stack Web Developer, Lockers Pvt. Ltd.");
-		System.out.println("Please select the folliwing options:");
-		System.out.println("1. Retrieve the filenames");
-		System.out.println("2. Perform CRUD file operations");
-		System.out.println("3. Navigate to the main menu");
 		
-		Scanner scanner = new Scanner(System.in);
 		int op, sop;
-		String strfilename, strfolder;
-		strfolder = "/Users/Neena/Documents/SimpliLearnFSD/SimplilearnPhase1Assessment/LockedMeProject/UserFiles/Pictures";
-		op = scanner.nextInt();
+		Scanner scanner = new Scanner(System.in);
+		op = Utilityoperations.mainMenu();
+
 		
-		List<String> files = new ArrayList<String>();
+		
+		String strfilename, strfolder, strwrite;
+		strfolder = "/Users/Neena/Documents/SimpliLearnFSD/SimplilearnPhase1Assessment/LockedMeProject/UserFiles/Pictures";
+		
+		
+		List<String> files = new LinkedList<String>();
 		files = Utilityoperations.getFilenames(strfolder);
-		Collections.sort(files);
+		Collections.sort(files, java.lang.String.CASE_INSENSITIVE_ORDER);
 		
 			switch(op)
 			{
@@ -45,6 +48,7 @@ public class WebUserOperations {
 				System.out.println("1. Add a file");
 				System.out.println("2. Delete a file");
 				System.out.println("3. Search a file ");
+				System.out.println("4. Go to the main menu ");
 				
 				sop = scanner.nextInt();
 				
@@ -52,20 +56,111 @@ public class WebUserOperations {
 				{
 					System.out.println("Please enter a filename to add: ");
 					strfilename = scanner.next().trim();
+					System.out.println("Please enter something to write to the file: ");
+					strwrite = scanner.next().trim().toLowerCase();
 					
 					try {
-						Utilityoperations.addFile(strfolder, strfilename);
-					} catch (FileNotFoundException e) {
+						 
+						for(String file:files)
+						{
+							if(!file.equals(strfilename))
+							{
+								Utilityoperations.addFile(strfolder, strfilename, strwrite);
+								files = Utilityoperations.getFilenames(strfolder);
+								files.add(strfilename);
+								Collections.sort(files, java.lang.String.CASE_INSENSITIVE_ORDER);
+								break;
+							}
+							else
+							{
+								System.out.println(strfilename + "- File already exists!");
+							}
+						}
+						
+							
+						
+
+					}
+					catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					
+				}
+				if(sop==2)
+				{
+					System.out.println("Please enter a filename to delele: ");
+					strfilename = scanner.next().trim();
+					File file = new File(strfolder + "/" + strfilename);
+					files = Utilityoperations.getFilenames(strfolder);
+					
+					
+					try
+					{
+						if(file.exists())
+						{
+									Utilityoperations.delFile(strfolder, strfilename);
+									files.remove(strfilename);
+								
+						}
+						else
+						{
+							System.out.println("Can't find file " + strfilename);
+						}
+						
+					}
+					catch(SecurityException ex)
+					{
+						System.out.println(ex.getMessage());
+					}
+				}
+				if(sop==3)
+				{
+					System.out.println("Search for a file: ");
+					strfilename = scanner.next().trim();
+					
+					File file = new File(strfolder + "/" + strfilename);
+					files = Utilityoperations.getFilenames(strfolder);
+					Collections.sort(files, java.lang.String.CASE_INSENSITIVE_ORDER);
+					
+					if(file.exists() || Collections.binarySearch(files, strfilename) >= 0)
+					{
+						System.out.println("Found the file!");
+						try(FileInputStream fis=new FileInputStream(strfolder + "/" + strfilename);
+								BufferedInputStream bis=new BufferedInputStream(fis);){ 
+						
+							StringBuilder sb=new StringBuilder();
+							while(bis.available()!=0) {
+								sb.append((char)bis.read());
+							}
+							System.out.println("Data read from the file is ");
+							System.out.println(sb.toString());
+																										
+							
+						} catch (FileNotFoundException e) {
+						System.out.println("");
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					else
+					{
+						System.out.println("File not found!");
+					}
 					
 				}
+				if(sop ==4)
+				{
+					op = Utilityoperations.mainMenu();
+				}
 				break;
+				
 			
 				
 			}
+			
+			
 		
 		
 		
